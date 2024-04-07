f7c371f75d48f063a1351baebf64524faa80e8c1
martinfrances107 <martinfrances107@hotmail.com>
2024-04-07 12:55:06 +0100

    Minor: examples/stippling ran prettier formatter.

--- examples/stippling/js/index.ts
+++ examples/stippling/js/index.ts
@@ -1,28 +1,27 @@
-// import { Renderer } from 'benchmark';
+const sizeRange = document.getElementById("size-range");
+const sizeLabel = document.getElementById("size-label");
+const perf = document.getElementById("perf");
 
-const sizeRange = document.getElementById('size-range');
-const sizeLabel = document.getElementById('size-label');
-const perf = document.getElementById('perf');
+import("../pkg").then((pkg) => {
+  console.log("wasm is imported");
 
-import('../pkg')
-  .then(pkg => {
-    console.log('wasm is imported');
+  const stippledCanvas = document
+    .querySelector("#stippled_canvas")
+    .transferControlToOffscreen();
 
-    const stippledCanvas = document.querySelector('#stippled_canvas').transferControlToOffscreen();
+  let stippler = pkg.main(stippledCanvas);
+  stippler.draw();
 
-    let stippler = pkg.main(stippledCanvas);
-    stippler.draw();
+  let k = 0;
+  const renderLoop = () => {
+    console.log("k={}", k);
+    stippler.next(k);
 
-    let k = 0;
-    const renderLoop = () => {
-      console.log("k={}",k);
-      stippler.next(k);
-
-      if (k < 80) {
-        requestAnimationFrame(renderLoop);
-        k++;
-      }
+    if (k < 80) {
+      requestAnimationFrame(renderLoop);
+      k++;
     }
+  };
 
-    renderLoop();
-  })
+  renderLoop();
+});
